6f8171d23c24f62ebb515527887005a5aaf1e150
denisadudas <dudasdenisaandrea@yahoo.ro>
2025-05-07 23:08:14 +0200

    updated taecher routes with promotion list and create

--- src/features/teacher/routes/TeacherRoutes.tsx
+++ src/features/teacher/routes/TeacherRoutes.tsx
@@ -1,10 +1,14 @@
 import { Route } from 'react-router-dom';
 import DashboardLayout from '../../../shared/layouts/DashboardLayout';
 import TeacherDashboard from '../dashboard/TeacherDashboard';
+import PromotionsPage from '../promotions/pages/PromotionPage';
+import PromotionCreatePage from '../promotions/pages/PromotionCreatePage';
 
 const TeacherRoutes = (
   <Route path="/teacher" element={<DashboardLayout role="teacher" />}>
     <Route index element={<TeacherDashboard />} />
+    <Route path="promotions" element={<PromotionsPage />} />
+      <Route path="promotions/create" element={<PromotionCreatePage />} />
   </Route>
 );
 
